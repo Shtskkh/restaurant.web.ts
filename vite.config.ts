b0f05b79ac54d2e0d8097865d6c5bdfef7591940
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-01 21:30:50 +0700

    Добавлен proxy для api

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import TanStackRouterVite from "@tanstack/router-plugin/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import TanStackRouterVite from "@tanstack/router-plugin/vite"; // https://vite.dev/config/
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        TanStackRouterVite({target: 'react', autoCodeSplitting: true}),
-        react()
-    ],
-})
+  plugins: [
+    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+    react(),
+  ],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://localhost:7280",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
