572b42e0f975dfd7de42900a5ae0eb8e168c990c
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-03 15:37:53 +0700

    Добавлен переход по страницам

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,12 +1,57 @@
 ﻿import {
+  Divider,
   Drawer,
   List,
   ListItem,
   ListItemButton,
+  ListItemIcon,
   ListItemText,
 } from "@mui/material";
+import PersonIcon from "@mui/icons-material/Person";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import ListIcon from "@mui/icons-material/List";
+import AccessibleForwardIcon from "@mui/icons-material/AccessibleForward";
+import InventoryIcon from "@mui/icons-material/Inventory";
+import RestaurantIcon from "@mui/icons-material/Restaurant";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { ReactNode } from "react";
+import { Link, useNavigate } from "@tanstack/react-router";
+import { auth } from "../contexts/auth.tsx";
+
+type ListItemLinkProps = {
+  to: string;
+  text: string;
+  icon?: ReactNode;
+};
+
+const ListItemLink = ({ to, icon, text }: ListItemLinkProps) => {
+  return (
+    <ListItem disableGutters>
+      <ListItemButton component={Link} href={to}>
+        {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
+        <ListItemText primary={text} />
+      </ListItemButton>
+    </ListItem>
+  );
+};
+
+const tabs: ListItemLinkProps[] = [
+  { to: "/profile", text: "Профиль", icon: <PersonIcon /> },
+  { to: "/dashboard", text: "Сводка", icon: <DashboardIcon /> },
+  { to: "/orders", text: "Заказы", icon: <ListIcon /> },
+  { to: "/dishes", text: "Блюда", icon: <RestaurantIcon /> },
+  { to: "/staff", text: "Сотрудники", icon: <AccessibleForwardIcon /> },
+  { to: "/supplies", text: "Поставки", icon: <InventoryIcon /> },
+];
 
 export const Sidebar = () => {
+  const navigate = useNavigate();
+
+  const handeLogOut = () => {
+    auth.signOut();
+    navigate({ to: "/login" }).then();
+  };
+
   return (
     <Drawer
       variant="permanent"
@@ -20,14 +65,21 @@ export const Sidebar = () => {
       }}
     >
       <List>
-        <ListItem>
-          <ListItemButton>
-            <ListItemText>Профиль</ListItemText>
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton>
-            <ListItemText>Выйти</ListItemText>
+        {tabs.map((tab) => (
+          <ListItemLink
+            key={tab.to}
+            to={tab.to}
+            text={tab.text}
+            icon={tab.icon}
+          />
+        ))}
+        <Divider />
+        <ListItem disableGutters>
+          <ListItemButton onClick={handeLogOut}>
+            <ListItemIcon>
+              <LogoutIcon />
+            </ListItemIcon>
+            <ListItemText primary="Выйти" />
           </ListItemButton>
         </ListItem>
       </List>
