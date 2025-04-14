eb648a5a595751933d712c299ee9c1bb7321b8bb
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-14 15:26:46 +0700

    Добавлена загрузка всей информации о заказе.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -10,12 +10,22 @@ export type User = {
   phoneNumber: string;
 };
 
+export type DishInOrder = {
+  idDish: number;
+  title: string;
+  count: number;
+  comment?: string;
+  status: string;
+  totalCost: number;
+};
+
 export type Order = {
   idOrder: number;
-  date: Date;
+  date: string;
   tableNumber: number;
   status: string;
   employee: string;
+  dishesInOrder: DishInOrder[];
 };
 
 export const fetchProfile = async (
@@ -47,6 +57,14 @@ export const fetchStaffById = async (id: string): Promise<User | undefined> => {
 export const fetchOrders = async (): Promise<Order[] | undefined> => {
   try {
     return (await axios.get<Order[]>("/api/orders")).data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const fetchOrder = async (id: string): Promise<Order | undefined> => {
+  try {
+    return (await axios.get<Order>(`/api/orders/${id}`)).data;
   } catch (error) {
     console.error(error);
   }
