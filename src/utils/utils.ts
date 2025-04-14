efd3d82cabc88c55ce3350a7eb622edc402f23dd
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-14 16:39:25 +0700

    Добавлено получение всех блюд и блюд по их id

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -28,6 +28,15 @@ export type Order = {
   dishesInOrder: DishInOrder[];
 };
 
+export type Dish = {
+  idDish: number;
+  title: string;
+  cost: number;
+  availability: boolean;
+  weightVolume: number;
+  unit: string;
+};
+
 export const fetchProfile = async (
   login: string,
 ): Promise<User | undefined> => {
@@ -65,6 +74,22 @@ export const fetchOrders = async (): Promise<Order[] | undefined> => {
 export const fetchOrder = async (id: string): Promise<Order | undefined> => {
   try {
     return (await axios.get<Order>(`/api/orders/${id}`)).data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const fetchDishes = async (): Promise<Dish[] | undefined> => {
+  try {
+    return (await axios.get<Dish[]>("/api/dishes")).data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const fetchDish = async (id: string): Promise<Dish | undefined> => {
+  try {
+    return (await axios.get<Dish>(`/api/dishes/${id}`)).data;
   } catch (error) {
     console.error(error);
   }
