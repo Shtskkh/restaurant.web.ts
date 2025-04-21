f492742c4e98de1c04626a98d2e68f36a1a91df7
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-21 13:22:34 +0700

    Создан отдельный файл для типов OpenAPI

---
+++ src/utils/types.ts
@@ -0,0 +1,7 @@
+﻿import { components } from "./schema";
+
+export type Employee = components["schemas"]["StaffModel"];
+export type Dish = components["schemas"]["DishModel"];
+export type Order = components["schemas"]["OrderModel"];
+export type DishInOrder = components["schemas"]["DishInOrderModel"];
+export type ProductInDishModel = components["schemas"]["ProductInDishModel"];
